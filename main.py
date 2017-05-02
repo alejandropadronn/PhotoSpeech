ef07658acf82dd00dc289a0f6a037f44cd958c9d
Pandeli Zi <Zi.Pandeli@gmail.com>
2017-05-02 18:54:38 -0400

    congiured gTTS(Google Text  to speech. refactored code into class for modularity

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 
+
 from api.google_cloud import VisualRecognition
 from camera.camera import Camera
+from tts.tts import TTS
 
 if __name__ == '__main__':
     # Prepare objects
     vr = VisualRecognition()
     camera = Camera()
+    tts = TTS(path='/home/pi/Desktop/PhotoSpeech/tts.mp3')
 
     photo_path = '/home/pi/Desktop/PhotoSpeech/photo.jpg'
     camera.start(photo_path)
@@ -19,3 +22,7 @@ if __name__ == '__main__':
     labels = vr.labels()
     for label in labels:
         print(label.description)
+    # ONLY FIRST LABEL FOR NOW
+    first_label = labels[0]
+    # Play first_label with english language, speed = fast/normal unless specified
+    tts.play(text=first_label, language='en')
--- tts/tts.py
+++ tts/tts.py
@@ -1,16 +1,14 @@
-from gtts import gTTS
-from time import sleep
 import os
-import pyglet
+
+from gtts import gTTS
 
 class TTS(object):
-    
-    def play(self, language, text, file_path):
-        gtts = gTTS(text=text, lang=language) # set Text,Language, and speed settings here (can be set to speak slower)
-        filename = file_path # temporary storage locaction for the .mp3 file
-        gtts.save(filename) # saves the file with the indicated name, in our case 'filename' is the name we've chosen to give our file.
 
-        music = pyglet.media.load(filename, streaming=False) # load the file and it's local AKA not streaming
-        music.play() # pretty self explanitory although this will play the indicated .mp3 with the indicated Text
-        sleep(music.duration) # will stop playing and go to sleep after music's .mp3 file has finished
-        os.remove(filename) # will remove the indicated file, from your computer since we only wanted it there for a moment anyway.
+    def __init__(self, path):
+        self.path = path
+
+    def play(self, text, language, slow=False):
+          tts = gTTS(text=text, lang=language, slow=slow)
+          tts.save(self.path)
+          os.system('mpg321 '+ self.path)
+          os.remove(self.path)
